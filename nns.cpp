a9076042c3f0a188ec8955ffffb6d4e3a1beabe1
Marcel Velez <marcel.velez1997@hotmail.com>
2018-06-06 15:56:11 +0200

    even pullen

--- nns.cpp
+++ nns.cpp
@@ -2,22 +2,23 @@
 #include <bitset>
 using namespace std;
 
+Typedef std::array<uint32_t, nw> bitvec_t ;
+Std:: vector<bitvector_t> list_t;
+Std::vector<std::pair<size_t, size_t>> output_t; (I)
+Typedef void(size_t, size_t)* fpair;
 
 
 
-int do_some_loopjes( int bit_vec1, int bit_vec2)
-{
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
+
+int do_some_loopjes( int bit_vec1, int bit_vec2) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < i; j++) {
             cout << std::bitset<16>((i*j*bit_vec1)^(i*j*bit_vec2)), cout << '\n';
         }
     }
 }
 
-int main()
-{
+int main() {
     int vec1, vec2;
     cout << "Hello, World!";
     cin >> vec1;
@@ -26,7 +27,11 @@ int main()
     return 0;
 }
 
-void NNS(L, t)  {
+bitvec_t generate_bitvec() {
+    
+}
+
+void NNS(L, t) {
     for (int i = 0; i < L.size(); i++) {
         for (int j = 0; j < i; j++) {
 
