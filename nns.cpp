edc78e03b029c052e3154d5c4ce85a8a7f1d89a8
David Vos <vosdavid98@gmail.com>
2018-06-06 13:14:32 +0200

    algo additions

--- nns.cpp
+++ nns.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int main() 
 {
-    cout << "Hello, Wosdsdaddarld!";
+    cout << "Hello, World!";
     return 0;
 }
 
 void NNS(L, t)  {
     for (int i = 0; i < L.size(); i++) {
-        
+        for (int j = 0; j < i; j++) {
+
+        }
     }
 }
