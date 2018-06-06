c52b01168769a3786e6af688d9e2fc18267e87a1
Marcel Velez <marcel.velez1997@hotmail.com>
2018-06-06 15:29:22 +0200

    een for loop en xor operatie op basis van user input

--- nns.cpp
+++ nns.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <bitset>
 using namespace std;
 
-int main() 
+
+
+
+int do_some_loopjes( int bit_vec1, int bit_vec2)
 {
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << std::bitset<16>((i*j*bit_vec1)^(i*j*bit_vec2)), cout << '\n';
+        }
+    }
+}
+
+int main()
+{
+    int vec1, vec2;
     cout << "Hello, World!";
+    cin >> vec1;
+    cin >> vec2;
+    cout << do_some_loopjes(vec1,vec2);
     return 0;
 }
