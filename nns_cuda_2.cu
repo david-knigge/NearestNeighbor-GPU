2f42eb78e5c5a042710835f4c31dab404f8b2737
david knigge <davidknigge@live.nl>
2018-06-18 11:05:40 +0200

    popc full vectors

--- nns_cuda_2.cu
+++ nns_cuda_2.cu
@@ -7,7 +7,7 @@
 #include "./generate_data.cpp"
 
 #define NW 8 // use bitvectors of d=NW*32 bits, example NW=8
-#define THREADS_PER_BLOCK 4 // Number of threads per block
+#define THREADS_PER_BLOCK 265 // Number of threads per block
 
 using std::uint32_t; // 32-bit unsigned integer used inside bitvector
 using std::size_t;   // unsigned integer for indices
@@ -37,6 +37,7 @@ __global__ void cuda_xor(uint32_t *vec_1, uint32_t *vecs, uint32_t *ret_vec)
         for (wordindex = 0; wordindex < NW; ++wordindex)
         {
             vectorweight += __popc(vecs[(*vec_1 * NW) + wordindex] ^ vecs[(vectorindex * NW) + wordindex]);
+            //printf("prim_vec: %d comp_vec: %d prim_word: %d comp_word: %d  score: %d vec_1: %d vec_2: %d xor: %d popc: %d\n", *vec_1, vectorindex, (*vec_1 * NW) + wordindex, (vectorindex * NW) + wordindex, vectorweight, vecs[(*vec_1 * NW) + wordindex], vecs[(vectorindex * NW) + wordindex],vecs[(*vec_1 * NW) + wordindex] ^ vecs[(vectorindex * NW) + wordindex], __popc(vecs[(*vec_1 * NW) + wordindex] ^ vecs[(vectorindex * NW) + wordindex]));
         }
         ret_vec[vectorindex] = vectorweight;
     }
@@ -48,10 +49,10 @@ inline size_t hammingweight(uint32_t n) {
 
 void printsomestuff(output_t output) {
     for (size_t i = 0; i < output.size(); i++) {
-        for (size_t j = 0; j < output[0].size(); j++) {
-            std::bitset<8> x(output[i][j]);
-        }
+        //printf("%zu,", output[i][0]);
+        //printf("%zu\n", output[i][1]);
     }
+    output.clear();
 }
 
 void NSS(const list_t& L, size_t t, callback_list_t f)  {
@@ -60,15 +61,25 @@ void NSS(const list_t& L, size_t t, callback_list_t f)  {
 
     // go over all unique pairs 0 <= j < i < L.size()
     bitvec_t *vecs;
-    uint32_t *vec, *vecd, *vecsd, *ret_vecd, *ret_vec; // device copies of a, b, c
+    uint32_t *vec, *vecd, *vecsd, *ret_vecd, *ret_vec, *ret_vec_zeroes;
     //int size = L.size() * sizeof(bitvec_t);
     int size = sizeof(bitvec_t);
 
     vec = (uint32_t *)malloc(sizeof(uint32_t));
     vecs = (bitvec_t *)malloc(sizeof(bitvec_t) * L.size());
-    ret_vec = (uint32_t *)malloc(sizeof(uint32_t) * L.size());
+    ret_vec = (uint32_t *)calloc(L.size(), sizeof(uint32_t));
+    ret_vec_zeroes = (uint32_t *)calloc(L.size(), sizeof(uint32_t));
 
-    memcpy(vecs, L.data(), sizeof(bitvec_t *));
+    memcpy(vecs, L.data(), L.size() * sizeof(bitvec_t));
+
+    // printf("copied_data:\n");
+    // for (int i = 0; i < L.size(); ++i)
+    // {
+    //     for(int j = 0; j < L[0].size(); ++j)
+    //     {
+    //         printf("vec: %d word: %d wordvalue: %d\n", i, j, ((uint32_t *)vecs)[i * 8 + j]);
+    //     }
+    // }
 
     // Allocate space for device copies of our primary vector, our entire setup
     // of vectors
@@ -82,12 +93,13 @@ void NSS(const list_t& L, size_t t, callback_list_t f)  {
     for (size_t i = 0; i < L.size(); ++i)    {
 
         *vec = i;
-        cudaMemcpy(vecd, vec, size, cudaMemcpyHostToDevice);
+        cudaMemcpy(ret_vecd, ret_vec_zeroes, L.size() * sizeof(uint32_t), cudaMemcpyHostToDevice);
+        cudaMemcpy(vecd, vec, sizeof(uint32_t), cudaMemcpyHostToDevice);
 
-        cuda_xor<<<((i + 1) + THREADS_PER_BLOCK-1) / THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(vecd, vecsd, ret_vecd);
+        cuda_xor<<<(i + THREADS_PER_BLOCK) / THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(vecd, vecsd, ret_vecd);
         cudaMemcpy(ret_vec, ret_vecd, L.size() * sizeof(uint32_t), cudaMemcpyDeviceToHost);
 
-        for (j = 0; j < i; ++j)
+        for (j = 0; j < i; j++)
         {
             if (ret_vec[j] < t)
             {
@@ -99,20 +111,29 @@ void NSS(const list_t& L, size_t t, callback_list_t f)  {
         }
         // periodically give outputlist back for further processing
         f(output); // assume it empties output
+        output.clear();
 
     }
     cudaFree(vecd); cudaFree(vecsd); cudaFree(ret_vecd);
-    free(vec); free(ret_vec); free(vecs);
+    free(vec); free(ret_vec); free(vecs); free(ret_vec_zeroes);
 }
 
 int main() {
     list_t test;
-    size_t leng = 10;
+    size_t leng = 10000;
     generate_random_list(test, leng);
-    size_t thersh = 98291;
-    cout << leng, cout << ' ';
+    size_t thersh = 128;
+    cout << leng, cout << '\n';
+    printf("Data:\n");
+    // for (int i = 0; i < test.size(); ++i)
+    // {
+    //     for(int j = 0; j < test[0].size(); ++j)
+    //     {
+    //         printf("vec: %d word: %d wordvalue: %d\n", i, j, ((uint32_t *)test.data())[i * 8 + j]);
+    //     }
+    // }
     NSS(test, thersh, printsomestuff);
-    cout << "klaar";
+    cout << "klaar\n";
     cout.flush();
     return 0;
 }
